216406ead320edc2fce32c0b67fd1b25fe10cf57
Mohammad Akbari <mohammad-akbari@hotmail.com>
2024-12-02 22:47:59 +0100

    chore: update telescope ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,11 +45,10 @@ return {
         prompt_prefix = "   ",
         initial_mode = "insert",
         selection_strategy = "reset",
-        sorting_strategy = "ascending",
         layout_strategy = "horizontal",
         layout_config = {
           horizontal = {
-            prompt_position = "top",
+            prompt_position = "bottom",
             preview_width = 0.55,
             results_width = 0.8,
           },
@@ -97,4 +96,3 @@ return {
     },
   },
 }
-
